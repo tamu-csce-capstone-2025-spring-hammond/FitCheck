8899e1346be9d3326a23406ac348780a9e819bdd
Li Shen <lililogan@gmail.com>
2025-04-15 14:58:46 -0500

    footer change

--- frontend/components/footer.tsx
+++ frontend/components/footer.tsx
@@ -2,8 +2,8 @@ export default function Footer() {
   return (
     <footer className=" _site-grid bg-black z-1">
       <div className="_grid-3">
-        <div className="flex justify-between align-middle py-6 text-body-light">
-          <p className="title">FitCheck 2025</p>
+        <div className="flex justify-between align-middle py-6 text-body-light text-lg">
+          <p className="bold text-lg">FitCheck 2025</p>
           <p>Created By HackInACap</p>
         </div>
       </div>
